a923bea3da147f5105e0825dc6a227fc85cdcd70
Katie Volk <volkk18@mail.wlu.edu>
2017-11-09 11:59:41 -0500

    Map work

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ library(ggplot2)
 breast_cancerrates2 <- read.table("breast_cancerrates2.txt", header= TRUE)
 gathered_data <- gather(breast_cancerrates2, key = "Race", value = "Incidence", 2:5, na.rm= FALSE)
 
+
 # Define server logic required to draw a histogram
 function(input, output) {
   
@@ -17,6 +18,24 @@ function(input, output) {
       theme(axis.text.x = element_text(angle = 60, hjust = 1))
   })  
   }
+
+#Server for map by county
+function(input, output) {
+  points <- breastcancer_bycounty_edited$County
+  
+  
+  output$bcmap <- renderLeaflet({
+    breastcancer_bycounty_edited %>%
+    leaflet() %>%
+      addProviderTiles(providers$OpenStreetMap)
+                       options = providerTileOptions(noWrap = TRUE) %>%
+                       addMarkers(lng= 39.8283, lat=98.5795, popup="Center of US")
+  
+  
+  
+  
+  })
+}
 
   
 
--- ui.R
+++ ui.R
@@ -22,7 +22,22 @@ mainPanel(
   plotOutput(outputId = "race_plot")
     )
   )
+    
 )
+
+#UI for Map of Incidence by County
+fluidPage(
+ titlePanel("Breast Cancer Map by County"),
+   
+ 
+ #Map for incidence by county
+   library(leaflet),
+     bcmap <- leaflet(data = breastcancer_bycounty_edited),
+        addTiles(bcmap),
+              setView(bcmap, 39.8283, 98.5795, zoom= 18),
+
+ 
+        )
 
 
 
