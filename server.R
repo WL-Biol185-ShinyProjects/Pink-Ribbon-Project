c40168bbd79ca0d88e53e085b903dbbee4c3256a
Kaleigh Hinton <hintonk18@mail.wlu.edu>
2017-12-07 12:29:42 -0500

    trying to update server and ui

--- server.R
+++ server.R
@@ -3,20 +3,18 @@ library(tidyverse)
 library(ggplot2)
 library(leaflet)
 
-<<<<<<< HEAD
-=======
 read.csv("gathered_data_corrected")
 gathered_data <- gather(breastcancerates2, key = "Race", value = "Incidence", 2:4, na.rm= TRUE)
 breast_cancerrates2 <- read.csv("gathered_data_corrected", header= TRUE)
 read.table("Final Breast Cancer Table")
 View(final_breastcancer)
->>>>>>> 2005394b19390f1944b0af9ab76c7311b93495e3
+
 
 # read_xlsx("death.xlsx")
 # gathered_death <- gather(death, key = "Race", value = "Deaths", 2:5, na.rm= TRUE)
 # 
 # 
-<<<<<<< HEAD
+
 # 
 # # Define server logic required to draw a histogram
 function(input, output) {
@@ -55,21 +53,6 @@ function(input, output) {
 # 
 # 
 # 
-# # function(input, output) {
-#     
-#     output$mymap <- renderLeaflet({
-#       final_breastcancer %>%
-#         na.omit(final_breastcancer) %>%
-#         filter(!is.na(longitude)) %>%
-#         filter(!is.na(latitude)) %>%
-#         filter(state == input$state) %>%
-#         arrange(county) %>%
-#         leaflet() %>% 
-#         setView(lng= -98, lat= 41, zoom= 4) %>%
-#         addTiles() %>%
-#         addMarkers(label = ~`Recent 5-Year Trend in Incidence Rates`, clusterOptions = markerClusterOptions())
-#     
-#     })
 
   
 
@@ -113,21 +96,7 @@ function(input, output) {
 # # zip_codes_states <-
 #    zip_codes_states %>%
 #    left_join(breastcancer_bycounty_edited2, by = c("county" = "County"))
-# 
-# 
-# 
-# 
-# 
-#      output$mymap <- renderLeaflet({
-#        zip_codes_states %>%
-#          filter(condition %in% input$condition)
-#          addTiles() %>%
-#          setView(lng = -79.442778, lat = 37.783889, zoom = 5) %>%
-#          addProviderTiles(providers$OpenStreetMap)
-#                           leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18)) %>%
-#        addMarkers(lng= 174.768, lat=-36.852, popup="The birthplace of R")
-#      })
-# 
+
 
 death_rate_time <- read_excel("~/Pink-Ribbon-Project/death_rate_time.xlsx")
 gathered_death <- gather(death_rate_time, key = "Age", value = "Rate", 2:4, na.rm = TRUE
